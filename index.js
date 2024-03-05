c630d808c771d24050bbc8b99564adb0cb2dc1c0
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-03-05 10:35:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const validUrl = require('valid-url');
 const shortid = require('shortid');
-
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Store mappings of short URLs to original URLs
+app.use('/public', express.static(`${process.cwd()}/public`));
+
 const urlDatabase = {};
 
-app.post('/api/shorturl', (req, res) => {
-  const { url } = req.body;
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-  // Check if the provided URL is valid
-  if (!validUrl.isUri(url)) {
-    return res.status(400).json({ error: 'invalid url' });
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', function(req, res) {
+  const originalUrl = req.body.url;
+
+  // Check if the URL is valid
+  if (!validUrl.isWebUri(originalUrl)) {
+    return res.json({ error: 'invalid url' });
   }
 
   // Generate short URL
   const shortUrl = shortid.generate();
 
-  // Store the mapping of short URL to original URL
-  urlDatabase[shortUrl] = url;
+  // Save original and short URLs to database
+  urlDatabase[shortUrl] = originalUrl;
 
-  // Send JSON response with original and short URLs
-  res.json({ original_url: url, short_url: shortUrl });
+  res.json({ original_url: originalUrl, short_url: shortUrl });
 });
 
-app.get('/api/shorturl/:shortUrl', (req, res) => {
-  const { shortUrl } = req.params;
-
-  // Check if the provided short URL exists in the database
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const shortUrl = req.params.short_url;
+  
+  // Check if the short URL exists in the database
   if (!urlDatabase.hasOwnProperty(shortUrl)) {
-    return res.status(404).json({ error: 'short url not found' });
+    return res.json({ error: 'invalid short url' });
   }
 
-  // Redirect to the original URL associated with the short URL
+  // Redirect to the original URL
   res.redirect(urlDatabase[shortUrl]);
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
